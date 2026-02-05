669280ac7d210196c9da62bf0591b151da4d719b
tiru_venkatesh <tiruvenkatesh123@gmail.com>
2026-02-05 06:48:50 +0530

    Update llm.py

--- services/llm.py
+++ services/llm.py
@@ -5,7 +5,7 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def call_llm(prompt: str):
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
+        model="gemini-1.5-flash-latest",
         contents=prompt
     )
     return response.text
